97325e1421ed17574bf2529130a05e37bfe3c096
Yuri Macena <yurimacena10@gmail.com>
2024-01-24 12:35:11 -0300

    Exercise completed

---
+++ JavaScript Básico/5/numbers.js
@@ -0,0 +1,7 @@
+window.alert('Bem vindo(a) ao meu site!')
+function botao(){
+    n1=window.prompt('Digite um número:')
+    dobro=n1*2
+    metade=n1/2
+    res.innerHTML=`O dobro de ${n1} é ${dobro} e a metade é ${metade}.`
+}
