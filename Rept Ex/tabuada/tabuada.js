1ff977b2d007ef7c34e1abd5a41c719412a5596e
Yuri Macena <yurimacena10@gmail.com>
2024-02-22 21:23:39 -0300

    Repraticando tabuada

--- Rept Ex/tabuada/tabuada.js
+++ Rept Ex/tabuada/tabuada.js
@@ -1,16 +1,16 @@
 function calcular(){
-    num=document.getElementById('txtnum')
-    res=document.getElementById('result')
-    if (num.value.length==0){
-        window.alert('Digite um número.')
+    num = document.getElementById('txtnum')
+    res = document.getElementById('result')
+    if (num.value.length == 0){
+        window.alert('Digite um número')
     } else {
-        n=Number(num.value)
-        res.innerHTML=''
+        n = Number(num.value)
+        res.innerHTML = ''
         for (count=1; count<=10; count++){
-        item=document.createElement('option')
-        item.innerHTML=`${n} x ${count} = ${n*count}`
-        item.value=`res${count}`
-        res.appendChild(item)
+            item = document.createElement('option')
+            item.innerHTML = `${n} x ${count} = ${n*count}`
+            item.value = `res${count}`
+            res.appendChild(item) 
         }
     }
 }
