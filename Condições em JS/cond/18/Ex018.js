fae38dc3cf3f533e0135be7bb98202b47f6cb2ba
Yuri Macena <yurimacena10@gmail.com>
2024-02-25 18:55:50 -0300

    Ex018 finalizado

---
+++ Condições em JS/cond/18/Ex018.js
@@ -0,0 +1,23 @@
+let res = document.getElementById('result')
+let comp = 0
+let player = 0
+
+function sortear(){
+    let min = 1
+    let max = 100
+    let dif = max - min
+    let randow = Math.random()
+    comp = min + Math.trunc(dif * randow)
+}
+
+function jogar(){
+    player = Number(window.prompt('Qual é o seu palpite?'))
+    if (player < comp) {
+        res.innerHTML += `<p>Você escreveu o número ${player}, meu número é <strong>MAIOR!</strong></p>`
+    } else if (player > comp) {
+        res.innerHTML += `<p>Você escreveu o número ${player}, meu número é <strong>MENOR!</strong></p>`
+    } else if (player == comp) {
+        res.innerHTML += `<p>Você escreveu o número ${player}, <strong>PARABÉNS!</strong> você acertou o meu número.</p>`
+        document.getElementById('botao').style.visibility = 'hidden'
+    }
+}
