b2935e28963692f7042dbc6a516303fc81f46ee1
Yuri Macena <yurimacena10@gmail.com>
2024-02-29 08:35:26 -0300

    Correção de possível resultado incorreto

--- Reps Ex/25/Ex025.js
+++ Reps Ex/25/Ex025.js
@@ -2,9 +2,13 @@ function calcular(){
     num = document.getElementById('txtnum')
     n = num.value
     res = document.getElementById('result')
-    res.innerHTML += `<h3>Contando de 0 até ${n}</h3>`
-    for (count = 0; count <= n; count++){
+    if (num.value.length == 0){
+        window.alert('Digite um número')
+    } else {
+        res.innerHTML += `<h3>Contando de 0 até ${n}</h3>`
+        for (count = 0; count <= n; count++){
         res.innerHTML += `${count} ➡️`
+        }
+        res.innerHTML += `🏁`
     }
-    res.innerHTML += `🏁`
 }
